83197edddca3b464a4bda9c9af6dd072eb305b98
gactocat <gactocat@gmail.com>
2016-06-13 22:08:53 +0900

    separate user handler

--- server.go
+++ server.go
@@ -1,58 +1,12 @@
 package main
 
 import (
-	"net/http"
-	"strconv"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/engine/standard"
 	"github.com/labstack/echo/middleware"
+	"github.com/gactocat/snowshoe/handler"
 )
-
-type (
-	user struct {
-		ID   int    `json:"id"`
-		Name string `json:"name"`
-	}
-)
-
-var (
-	users = map[int]*user{}
-	seq   = 1
-)
-
-func createUser(c echo.Context) error {
-	u := &user{
-		ID: seq,
-	}
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	users[u.ID] = u
-	seq++
-	return c.JSON(http.StatusCreated, u)
-}
-
-func getUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	return c.JSON(http.StatusOK, users[id])
-}
-
-func updateUser(c echo.Context) error {
-	u := new(user)
-	if err := c.Bind(u); err != nil {
-		return err
-	}
-	id, _ := strconv.Atoi(c.Param("id"))
-	users[id].Name = u.Name
-	return c.JSON(http.StatusOK, users[id])
-}
-
-func deleteUser(c echo.Context) error {
-	id, _ := strconv.Atoi(c.Param("id"))
-	delete(users, id)
-	return c.NoContent(http.StatusNoContent)
-}
 
 func main() {
 	e := echo.New()
@@ -62,10 +16,10 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routers
-	e.POST("users", createUser)
-	e.GET("/users/:id", getUser)
-	e.PUT("/users/:id", updateUser)
-	e.DELETE("/users/:id", deleteUser)
+	e.POST("users", handler.CreateUser)
+	e.GET("/users/:id", handler.GetUser)
+	e.PUT("/users/:id", handler.UpdateUser)
+	e.DELETE("/users/:id", handler.DeleteUser)
 
 	// Start server
 	e.Run(standard.New(":1323"))
